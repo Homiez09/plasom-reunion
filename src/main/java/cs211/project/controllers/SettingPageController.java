a0ca8fbb2496c463476e6df5b543adc7fca1cf84
OwlVi <mrowlvi@gmail.com>
2023-08-10 19:08:01 +0700

    toggle switch

--- src/main/java/cs211/project/controllers/SettingPageController.java
+++ src/main/java/cs211/project/controllers/SettingPageController.java
@@ -4,6 +4,8 @@ import cs211.project.services.FXRouter;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
@@ -15,6 +17,12 @@ public class SettingPageController {
     AnchorPane navbarAnchorPane;
     @FXML
     Pane mainPane,loginsercurityPane,interfacePane;
+    @FXML
+    ToggleButton privateaccountToggleButton;
+    @FXML
+    ImageView offImageView,onImageView,switchImageView;
+
+    private boolean isClicked = false;
 
     private void loadTopBarComponent() {
         FXMLLoader topBarComponentLoader = new FXMLLoader(getClass().getResource("/cs211/project/views/components/navbar.fxml"));
@@ -29,10 +37,22 @@ public class SettingPageController {
     @FXML
     public void initialize(){
         mainPane.setVisible(true);
+        ToggleOff();
+
         loadTopBarComponent();
 
     }
+    public void ToggleOn(){
+        offImageView.setVisible(false);
+        onImageView.setVisible(true);
+        switchImageView.setLayoutX(30);
+    }
 
+    public void ToggleOff(){
+        offImageView.setVisible(true);
+        onImageView.setVisible(false);
+        switchImageView.setLayoutX(0);
+    }
     private void ShowPaneInterface(){
         mainPane.setVisible(false);
         loginsercurityPane.setVisible(false);
@@ -51,5 +71,16 @@ public class SettingPageController {
 
     public void onLoginButton(ActionEvent actionEvent) {
         ShowPaneLogin();
+    }
+
+    public void onPaneClick(MouseEvent mouseEvent) {
+        if (isClicked){
+            isClicked = false;
+            ToggleOff();
+        }else {
+            isClicked = true;
+            ToggleOn();
+        }
+
     }
 }
