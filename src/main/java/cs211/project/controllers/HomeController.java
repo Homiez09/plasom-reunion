1baf32cc164b05f46810f42f9193083fcc20f6b5
JindamaneeSri <jindamanee.sri@ku.th>
2023-08-10 14:22:43 +0700

    import navbar into home-guest.fxml

--- src/main/java/cs211/project/controllers/HomeController.java
+++ src/main/java/cs211/project/controllers/HomeController.java
@@ -2,12 +2,20 @@ package cs211.project.controllers;
 
 import cs211.project.services.FXRouter;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
 
 import java.io.IOException;
 
 public class HomeController {
+    @FXML private AnchorPane navbarAnchorPane;
+    @FXML private HBox navbarGuest;
     @FXML
-    private  void initialize() {}
+    private  void initialize() {
+        loadNavbarComponent();
+        navbarGuest.setVisible(false);
+    }
 
     @FXML
     protected void onSignUpButtonClick()
@@ -19,6 +27,15 @@ public class HomeController {
         }
     }
 
+    private void loadNavbarComponent() {
+        FXMLLoader navbarComponentLoader = new FXMLLoader(getClass().getResource("/cs211/project/views/components/navbar.fxml"));
+        try {
+            AnchorPane topBarComponent = navbarComponentLoader.load();
+            navbarAnchorPane.getChildren().add(topBarComponent);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
     @FXML
     protected void onSignInButtonClick()
     {
