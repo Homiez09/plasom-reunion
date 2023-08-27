da16d544068de2f13160f45701235aa75237bbd8
OwlVi <mrowlvi@gmail.com>
2023-08-27 20:03:01 +0700

    Event Classà¹à¸

--- src/main/java/cs211/project/controllers/MyEventController.java
+++ src/main/java/cs211/project/controllers/MyEventController.java
@@ -1,7 +1,16 @@
 package cs211.project.controllers;
 
+import cs211.project.models.Event;
+import cs211.project.models.collections.EventList;
+import cs211.project.services.Datasource;
+import cs211.project.services.EventDataSourceHardCode;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 
 import java.io.IOException;
@@ -9,6 +18,45 @@ import java.io.IOException;
 public class MyEventController {
     @FXML
     AnchorPane navbarAnchorPane;
+    @FXML
+    Label usernameLabel;
+    @FXML
+    ImageView userImageView;
+    @FXML
+    ListView myeventListView;
+
+    private EventList eventList;
+    private Event selectEvent;
+    private Datasource<EventList> datasourcecsv;
+
+
+    @FXML
+    public void initialize(){
+        loadTopBarComponent();
+        EventDataSourceHardCode datasource = new EventDataSourceHardCode();
+        eventList = datasource.readData();
+        showlist(eventList);
+        myeventListView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Event>() {
+            @Override
+            public void changed(ObservableValue<? extends Event> observableValue, Event oldValue, Event newValue) {
+                if (newValue == null){
+                    selectEvent = null;
+                }else {
+                    selectEvent = newValue;
+                }
+            }
+        });
+
+
+    }
+
+    private void showlist(EventList eventList){
+        loadTopBarComponent();
+        myeventListView.getItems().clear();
+        myeventListView.getItems().addAll(eventList.getEvents());
+
+    }
+
     // top-bar
     private void loadTopBarComponent() {
         FXMLLoader topBarComponentLoader = new FXMLLoader(getClass().getResource("/cs211/project/views/components/navbar.fxml"));
@@ -19,12 +67,5 @@ public class MyEventController {
             throw new RuntimeException(e);
         }
     }
-    @FXML
-    public void initialize(){
-        loadTopBarComponent();
-
-    }
-
-
 
 }
---
+++ src/main/java/cs211/project/models/collections/EventList.java
@@ -0,0 +1,37 @@
+package cs211.project.models.collections;
+
+import cs211.project.models.Event;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class EventList<T> {
+    private ArrayList<Event> events;
+
+    public EventList() {
+        events = new ArrayList<>();
+    }
+
+    public void addEvent(String eventName, String eventImagePath, String eventDateStart, String eventDateEnd, String eventDescription, int slotMember) {
+        eventName = eventName.trim();
+        if (!eventName.equals("") ){
+            events.add(new Event(eventName,eventImagePath,eventDateStart,eventDateEnd,eventDescription,slotMember));
+        }
+    }
+
+    public Event findEvent(String name) {
+        for (Event event : events) {
+            if (event.getEventName().equals(name)) {
+                return event;
+            }
+        }
+        return null;
+    }
+
+    public ArrayList<Event> getEvents() {
+        return events;
+    }
+
+
+}
