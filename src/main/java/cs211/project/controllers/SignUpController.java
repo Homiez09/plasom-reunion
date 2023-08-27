4a05a6db6d1be33a2c8fdf3c7c9031c51d2bb7bd
MalaMing <minggggggg@gmail.com>
2023-08-28 05:01:54 +0700

    updated validate and match password checked function and FXML

--- src/main/java/cs211/project/controllers/SignUpController.java
+++ src/main/java/cs211/project/controllers/SignUpController.java
@@ -1,9 +1,11 @@
 package cs211.project.controllers;
 
+import cs211.project.models.User;
 import cs211.project.services.FXRouter;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
@@ -20,27 +22,35 @@ public class SignUpController {
 
     @FXML
     private Button backButton, nextButton;
-    @FXML private Shape backCircle, nextCircle;
+    @FXML
+    private Shape backCircle, nextCircle, passwordRequireBox, passwordRequireBoxLabel;
 
-    @FXML private PasswordField passwordField,confirmPasswordField;
-    @FXML private TextField showPasswordTextField,showConfirmPasswordTextField;
+    @FXML
+    private Label passwordRequireLabel, passwordLengthReq, passwordUpperCaseReq, passwordLowerCaseReq, passwordSpecialReq, passwordNumReq;
 
-    @FXML private ImageView upComingEventsImageView, signBackgroundImageView, upComingEventsBackgroundImageView;
-    @FXML private ImageView fullNameIconView, usernameIconView, passwordIconView, confirmPasswordIconView, visiblePasswordImageView, visibleConfirmPasswordImageView;
-    private Image showPasswordImage, hidePasswordImage;
+    @FXML
+    private PasswordField passwordField, confirmPasswordField;
+    @FXML
+    private TextField showPasswordTextField, showConfirmPasswordTextField, DisplayNameTextfield, usernameTextField;
+
+    @FXML
+    private ImageView upComingEventsImageView, signBackgroundImageView, upComingEventsBackgroundImageView;
+    @FXML
+    private ImageView fullNameIconView, usernameIconView, passwordIconView, confirmPasswordIconView, visiblePasswordImageView,checkBoxPasswordImageView,checkBoxConfirmPasswordImageView;
+    private Image showPasswordImage, hidePasswordImage,checkBoxPasswordIcon;
     private String password, confirmPassword;
+    private boolean isHandlingFocusChange = false,passwordMatching =false;
 
     @FXML
     void initialize() {
-        loadImage();
-        showPasswordTextField.setVisible(false);
-        visiblePasswordImageView.setImage(hidePasswordImage);
-        showConfirmPasswordTextField.setVisible(false);
-        visibleConfirmPasswordImageView.setImage(hidePasswordImage);
 
-        maxPage = calculateMaxPage();
+        showFocusRequirementsPassword();
+        loadImage();
+        setTextFieldVisible(false);
         updateVisibleButton();
         showImage(page);
+        maxPage = calculateMaxPage();
+
     }
 
     private void updateVisibleButton() {
@@ -67,7 +77,7 @@ public class SignUpController {
     }
 
     @FXML
-    protected void onSignInClick(){
+    protected void onSignInClick() {
         try {
             FXRouter.goTo("sign-in");
         } catch (IOException e) {
@@ -76,7 +86,7 @@ public class SignUpController {
     }
 
     @FXML
-    protected void onBackClick(){
+    protected void onBackClick() {
         try {
             FXRouter.goTo("welcome");
         } catch (IOException e) {
@@ -84,47 +94,136 @@ public class SignUpController {
         }
     }
 
+    private void showFocusRequirementsPassword() {
+
+        if (isHandlingFocusChange) {
+            return;
+        }
+
+        isHandlingFocusChange = true;
+
+        if (passwordField.isFocused() || showPasswordTextField.isFocused()) {
+            showRequirementPassword(true);
+        } else {
+            showRequirementPassword(false);
+        }
+
+        passwordField.focusedProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue) {
+                showFocusRequirementsPassword();
+            }
+        });
+        showPasswordTextField.focusedProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue) {
+                showFocusRequirementsPassword();
+            }
+        });
+
+        isHandlingFocusChange = false;
+    }
+
+    private void showRequirementPassword(boolean require){
+        passwordRequireBoxLabel.setVisible(require);
+        passwordRequireBox.setVisible(require);
+        passwordRequireLabel.setVisible(require);
+        passwordLengthReq.setVisible(require);
+        passwordUpperCaseReq.setVisible(require);
+        passwordLowerCaseReq.setVisible(require);
+        passwordSpecialReq.setVisible(require);
+        passwordNumReq.setVisible(require);
+    }
+
+
     public void onKeyHidePassword(KeyEvent keyEvent) {
         password = passwordField.getText();
         showPasswordTextField.setText(password);
+        checkPasswordRequirement();
     }
 
     public void onKeyShowPassword(KeyEvent keyEvent) {
         password = showPasswordTextField.getText();
         passwordField.setText(password);
+        checkPasswordRequirement();
     }
+
+
+    private void checkPasswordRequirement() {
+        boolean hasUpperCase = false, hasLowerCase = false, hasDigit = false, hasSpecialCharacter = false , hasFitLength = false;
+        String specialCharacters = "!@#$";
+
+        if(password.length() >= 8 && password.length() <= 20){
+            hasFitLength = true;
+        }
+        for (char p : password.toCharArray()) {
+            if (Character.isUpperCase(p)) {
+                hasUpperCase = true;
+            } else if (Character.isLowerCase(p)) {
+                hasLowerCase = true;
+            } else if (Character.isDigit(p)) {
+                hasDigit = true;
+            } else if (specialCharacters.contains(String.valueOf(p))) {
+                hasSpecialCharacter = true;
+            }
+        }
+
+        passwordLengthReq.setStyle( hasFitLength? "-fx-text-fill: #70C050" : "-fx-text-fill: red");
+        passwordUpperCaseReq.setStyle(hasUpperCase ? "-fx-text-fill: #70C050" : "-fx-text-fill: red");
+        passwordLowerCaseReq.setStyle(hasLowerCase ? "-fx-text-fill: #70C050" : "-fx-text-fill: red");
+        passwordNumReq.setStyle(hasDigit ? "-fx-text-fill: #70C050" : "-fx-text-fill: red");
+        passwordSpecialReq.setStyle(hasSpecialCharacter ? "-fx-text-fill: #70C050" : "-fx-text-fill: red");
+
+        if(hasFitLength && hasLowerCase && hasDigit && hasUpperCase && hasSpecialCharacter){
+            passwordMatching = true;
+            showRequirementPassword(false);
+            checkBoxPasswordImageView.setImage(checkBoxPasswordIcon);
+            checkBoxPasswordImageView.setVisible(true);
+            if(password.equals(confirmPassword)){
+                checkBoxConfirmPasswordImageView.setVisible(true);
+                checkBoxConfirmPasswordImageView.setImage(checkBoxPasswordIcon);
+            }else {
+                checkBoxConfirmPasswordImageView.setVisible(false);
+            }
+        }else{
+            checkBoxConfirmPasswordImageView.setVisible(false);
+            checkBoxPasswordImageView.setVisible(false);
+            passwordMatching = false;
+            showRequirementPassword(true);
+        }
+
+
+    }
+
 
     public void onKeyHideConfirmPassword(KeyEvent keyEvent) {
         confirmPassword = confirmPasswordField.getText();
         showConfirmPasswordTextField.setText(confirmPassword);
+        checkPasswordRequirement();
     }
 
     public void onKeyShowConfirmPassword(KeyEvent keyEvent) {
         confirmPassword = showConfirmPasswordTextField.getText();
         confirmPasswordField.setText(confirmPassword);
+        checkPasswordRequirement();
+    }
+
+    private void setTextFieldVisible(boolean visible){
+        showPasswordTextField.setVisible(visible);
+        showConfirmPasswordTextField.setVisible(visible);
     }
 
     @FXML
     private void onVisiblePasswordClick(MouseEvent event) {
         if (visiblePasswordImageView.getImage() == hidePasswordImage) {
-            showPasswordTextField.setVisible(true);
+            setTextFieldVisible(true);
             visiblePasswordImageView.setImage(showPasswordImage);
         } else {
-            showPasswordTextField.setVisible(false);
+            setTextFieldVisible(false);
             visiblePasswordImageView.setImage(hidePasswordImage);
+
         }
     }
 
-    @FXML
-    private void onVisibleConfirmPasswordClick(MouseEvent event) {
-        if (visibleConfirmPasswordImageView.getImage() == hidePasswordImage) {
-            showConfirmPasswordTextField.setVisible(true);
-            visibleConfirmPasswordImageView.setImage(showPasswordImage);
-        } else {
-            showConfirmPasswordTextField.setVisible(false);
-            visibleConfirmPasswordImageView.setImage(hidePasswordImage);
-        }
-    }
+
 
     private int calculateMaxPage() {
         int countImage = 0;
@@ -154,11 +253,12 @@ public class SignUpController {
         passwordIconView.setImage(passwordIcon);
         confirmPasswordIconView.setImage(passwordIcon);
 
+        checkBoxPasswordIcon = new Image(getClass().getResourceAsStream("/images/icons/login/checkbox_password.png"));
         showPasswordImage = new Image(getClass().getResourceAsStream("/images/icons/login/show_password.png"));
         hidePasswordImage = new Image(getClass().getResourceAsStream("/images/icons/login/hide_password.png"));
 
         visiblePasswordImageView.setImage(hidePasswordImage);
-        visibleConfirmPasswordImageView.setImage(hidePasswordImage);
+
     }
 
     private void showImage(int pageNumber) {
@@ -168,11 +268,24 @@ public class SignUpController {
         updateVisibleButton();
     }
 
-    public void onCreateAccountButton(ActionEvent actionEvent) {
-        try {
-            FXRouter.goTo("home");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+    private boolean validatePasswordConfirmation(){
+        return (password.equals(confirmPassword) && passwordMatching);
     }
+
+    public void onCreateAccountButton(ActionEvent actionEvent) {
+        if(validatePasswordConfirmation()){
+            User user = new User(DisplayNameTextfield.getText(), usernameTextField.getText(),password); // todo:hash password!
+            try {
+                FXRouter.goTo("sign-in", user);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }else{
+            System.out.println("wrong pass");
+            return ;
+            // todo: error!
+        }
+
+    }
+
 }
