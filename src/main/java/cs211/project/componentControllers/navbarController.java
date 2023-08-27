1a8a32916edeacd5ba1c1e1c4badadc353b352cc
Phumrapee Soenvanichakul <phumsoen123@gmail.com>
2023-08-27 16:48:04 +0700

    updated navbar

--- src/main/java/cs211/project/componentControllers/navbarController.java
+++ src/main/java/cs211/project/componentControllers/navbarController.java
@@ -19,9 +19,10 @@ public class navbarController {
     }
 
     private void showProfile() {
-        Image profile = new Image(getClass().getResource("/images/profiles-dev/ming.jpg").toString(), 1280, 1280, true, false);
+        Image profile = new Image(getClass().getResource("/images/profiles-dev/ming.jpg").toString(), 1280, 1280, false, false);
         profileImageView.setImage(profile);
         profileImageView.setFitWidth(58);
+        profileImageView.setFitHeight(58);
         profileImageView.setClip(getProfileCircle(profileImageView));
     }
 
