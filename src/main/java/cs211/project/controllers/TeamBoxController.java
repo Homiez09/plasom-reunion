1896d480f503253e40d4d130033e0418eac5c292
MalaMing <minggggggg@gmail.com>
2023-08-30 02:05:18 +0700

    add team box controller

---
+++ src/main/java/cs211/project/controllers/TeamBoxController.java
@@ -0,0 +1,38 @@
+package cs211.project.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class TeamBoxController {
+
+    @FXML
+    private ImageView bookMarkImageView;
+
+
+    @FXML
+    private ImageView peopleImageView, roleImageView, activeImageView, faceImageView;
+
+    @FXML
+    private Label numActiveLabel, teamNameLabel;
+
+    @FXML private List<Team>
+
+    @FXML private void initialize(){
+        loadIcon();
+    }
+
+    private void loadIcon(){
+        Image bookMarkIcon = new Image(getClass().getResourceAsStream("/images/icons/team-box/bookmark_icon.png"));
+        bookMarkImageView.setImage(bookMarkIcon);
+        Image activeIcon = new Image(getClass().getResourceAsStream("/images/icons/team-box/active_icon.png"));
+        activeImageView.setImage(activeIcon);
+        Image dotIcon = new Image(getClass().getResourceAsStream("/images/icons/team-box/dot_icon.png"));
+        activeImageView.setImage(dotIcon);
+        Image faceIcon = new Image(getClass().getResourceAsStream("/images/icons/team-box/face_icon.png"));
+        faceImageView.setImage(faceIcon);
+        Image peopleIcon = new Image(getClass().getResourceAsStream("/images/icons/team-box/people_icon.png"));
+        peopleImageView.setImage(peopleIcon);
+    }
+}
