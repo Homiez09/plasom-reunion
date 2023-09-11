4da4b686180e2657cb4a146b818babc0851a2ddf
MalaMing <minggggggg@gmail.com>
2023-09-12 03:48:28 +0700

    add materials

--- src/main/java/cs211/project/componentControllers/TeamBox1Controller.java
+++ src/main/java/cs211/project/componentControllers/TeamBox1Controller.java
@@ -9,15 +9,26 @@ public class TeamBoxController {
 
     @FXML private ImageView pointImageView, peopleImageView, roleImageView, activeImageView, faceImageView, bookMarkImageView, manageTeamImageView;
     @FXML private Label numActiveLabel, teamNameLabel;
+    private Image unBookMarkIcon, bookMarkIcon;
 
 
     @FXML private void initialize() {
+
         loadIcon();
     }
 
+    @FXML
+    private void onBookMarkClick(){
+        if(bookMarkImageView.getImage() == unBookMarkIcon) {
+            bookMarkImageView.setImage(bookMarkIcon);
+        }else {
+        bookMarkImageView.setImage(unBookMarkIcon);
+        }
+    }
+
     private void loadIcon() {
-        Image bookMarkIcon = new Image(getClass().getResourceAsStream("/images/icons/team-box/bookmark_icon.png"));
-        bookMarkImageView.setImage(bookMarkIcon);
+        unBookMarkIcon = new Image(getClass().getResourceAsStream("/images/icons/team-box/bookmark/unbookmark_icon.png"));
+        bookMarkIcon = new Image(getClass().getResourceAsStream("/images/icons/team-box/bookmark/bookmark_icon.png"));
         Image activeIcon = new Image(getClass().getResourceAsStream("/images/icons/team-box/active_icon.png"));
         activeImageView.setImage(activeIcon);
         Image roleIcon = new Image(getClass().getResourceAsStream("/images/icons/team-box/role/member.png"));
@@ -28,6 +39,9 @@ public class TeamBoxController {
         peopleImageView.setImage(peopleIcon);
         Image manageIcon = new Image(getClass().getResourceAsStream("/images/icons/team-box/dot_icon.png"));
         manageTeamImageView.setImage(manageIcon);
+
+        bookMarkImageView.setImage(unBookMarkIcon);
+
 
     }
 
