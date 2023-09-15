fbf4beaac0c3061c149cb24b0c40a697327d0b01
OwlVi <mrowlvi@gmail.com>
2023-09-15 23:03:30 +0700

    Add Check func

--- src/main/java/cs211/project/componentControllers/ChangePassController.java
+++ src/main/java/cs211/project/componentControllers/ChangePassController.java
@@ -6,14 +6,19 @@ import cs211.project.models.collections.UserList;
 import cs211.project.services.Datasource;
 import cs211.project.services.FXRouter;
 import cs211.project.services.UserListDataSource;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.stage.Modality;
 
 public class ChangePassController {
+    @FXML
+    Label currentErrorLabel,newErrorLabel,reNewErrorLabel;
     @FXML
     PasswordField currentPasswordField, newPasswordField, reNewPasswordField;
     private Datasource<UserList> userListDatasource;
@@ -25,33 +30,75 @@ public class ChangePassController {
         this.currentUser = (User) FXRouter.getData();
         this.userListDatasource = new UserListDataSource("data", "user-list.csv");
         this.userList = userListDatasource.readData();
-        System.out.println(currentUser.getUsername());
+        hideErrorLabel();
 
+        currentPasswordField.textProperty().addListener((observableValue, oldValue, newValue) -> {
+            validateCurrentPassword(newValue);
+            currentErrorLabel.setVisible(!newValue.isEmpty());
+        });
+
+        newPasswordField.textProperty().addListener((observableValue, oldValue, newValue) -> {
+            validateNewPassword(newValue);
+            newErrorLabel.setVisible(!newValue.isEmpty());
+
+        });
+
+        reNewPasswordField.textProperty().addListener((observableValue, oldValue, newValue) -> {
+            validateReNewPassword(newValue);
+            reNewErrorLabel.setVisible(!newValue.isEmpty());
+
+        });
 
     }
 
+
+    private void validateCurrentPassword(String value) {
+        // เช็ครหัสผ่านเดิมที่ผู้ใช้ป้อน
+        boolean isValid = checkPassword();
+        if (!isValid && !value.isEmpty()) {
+            currentErrorLabel.setText("รหัสผ่านปัจจุบันไม่ถูกต้อง");
+        } else {
+            currentErrorLabel.setText("");
+        }
+    }
+
+    private void validateNewPassword(String value) {
+        // เช็ครหัสผ่านใหม่ที่ผู้ใช้ป้อน
+        if (value.length() < 8 && !value.isEmpty()) {
+            newErrorLabel.setText("รหัสผ่านต้องมีความยาวอย่างน้อย 8 ตัวอักษร");
+        } else {
+            newErrorLabel.setText("");
+        }
+    }
+
+    private void validateReNewPassword(String value) {
+        // เช็ครหัสผ่านใหม่อีกครั้งที่ผู้ใช้ป้อน
+        if (!value.equals(newPasswordField.getText()) && !value.isEmpty()) {
+            reNewErrorLabel.setText("รหัสผ่านใหม่ไม่ตรงกับรหัสผ่านใหม่อีกครั้ง");
+        } else {
+            reNewErrorLabel.setText("");
+        }
+    }
     private boolean checkPassword() {
         return BCrypt.verifyer().verify(currentPasswordField.getText().toCharArray(), currentUser.getPassword()).verified;
     }
-
+    private boolean checkNewPassword(){
+        String newpass = newPasswordField.getText();
+        String renewpass = reNewPasswordField.getText();
+    return newpass.equals(renewpass);
+    }
     private String setPassword(String password) {
         return BCrypt.withDefaults().hashToString(12, password.toCharArray());
     }
 
-    private void setNewPassword() {
+    private void setNewPassword(User user) {
         String newpass = newPasswordField.getText();
-        String renewpass = reNewPasswordField.getText();
-        if (newpass.equals(renewpass)) {
-            currentUser.setPassword(setPassword(newpass));
-        }
+        user.setPassword(setPassword(newpass));
+
     }
 
-    private boolean samePass(){
-        Alert alert = new Alert(Alert.AlertType.WARNING);
-        alert.setTitle("Using Same Password");
-        alert.setContentText("Try Use New Password");
-        if (currentPasswordField.getText().equals(newPasswordField)){
-            alert.showAndWait();
+    private boolean samePassword(){
+        if (currentPasswordField.getText().equals(newPasswordField.getText())&& !currentPasswordField.getText().isEmpty()){
             newPasswordField.setText("");
             reNewPasswordField.setText("");
             return false;
@@ -65,25 +112,43 @@ public class ChangePassController {
     }
 
     public void onChangeAction(ActionEvent actionEvent) {
-        if (samePass()){
-            Alert confirmationAlert = new Alert(Alert.AlertType.CONFIRMATION);
-            confirmationAlert.setTitle("ยืนยันการเปลี่ยนรหัสผ่าน");
-            confirmationAlert.setHeaderText(null);
-            confirmationAlert.setContentText("คุณต้องการเปลี่ยนรหัสผ่านของคุณใช่หรือไม่?");
+        Alert alert = new Alert(Alert.AlertType.WARNING);
+        if (!samePassword()) {
+            if (checkNewPassword()) {
+                Alert confirmationAlert = new Alert(Alert.AlertType.CONFIRMATION);
+                confirmationAlert.setTitle("ยืนยันการเปลี่ยนรหัสผ่าน");
+                confirmationAlert.setHeaderText(null);
+                confirmationAlert.setContentText("คุณต้องการเปลี่ยนรหัสผ่านของคุณใช่หรือไม่?");
 
-            // เราจะใช้ lambda expression เพื่อตรวจสอบผลลัพธ์ที่ผู้ใช้เลือก
-            confirmationAlert.initModality(Modality.APPLICATION_MODAL);
-            confirmationAlert.initOwner(currentPasswordField.getScene().getWindow());
+                // เราจะใช้ lambda expression เพื่อตรวจสอบผลลัพธ์ที่ผู้ใช้เลือก
+                confirmationAlert.initModality(Modality.APPLICATION_MODAL);
+                confirmationAlert.initOwner(currentPasswordField.getScene().getWindow());
 
-            confirmationAlert.showAndWait().ifPresent(response -> {
-                if (response == ButtonType.OK && checkPassword()) {
-                    // ผู้ใช้เลือก "ใช่" และรหัสผ่านถูกต้อง
-                    // ทำการเปลี่ยนรหัสผ่านและบันทึกข้อมูลที่นี่
-                    setNewPassword();
-                    userList.findUserId(currentUser.getUserId());
-                    userListDatasource.writeData(userList);
-                }
-            });
+                confirmationAlert.showAndWait().ifPresent(response -> {
+                    if (response == ButtonType.OK && checkPassword()) {
+                        Alert alertinfo = new Alert(Alert.AlertType.INFORMATION);
+                        alertinfo.setContentText("Success");
+                        User user = userList.findUserId(currentUser.getUserId());
+                        setNewPassword(user);
+                        userListDatasource.writeData(userList);
+                        alertinfo.showAndWait();
+                    }
+
+                });
+            }else {
+                alert.setTitle("Password not correct");
+                alert.setContentText("Try Again");
+                alert.showAndWait();
+            }
+        }else {
+            alert.setTitle("Using Same Password");
+            alert.setContentText("Try Use New Password");
+            alert.showAndWait();
         }
+    }
+    private void hideErrorLabel(){
+        currentErrorLabel.setVisible(false);
+        reNewErrorLabel.setVisible(false);
+        newErrorLabel.setVisible(false);
     }
 }
