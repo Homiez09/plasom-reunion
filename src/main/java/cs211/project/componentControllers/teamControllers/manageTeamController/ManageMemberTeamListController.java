e674a43ae8631220a924691a4333411e715c5639
MalaMing <minggggggg@gmail.com>
2023-09-21 16:49:06 +0700

    add manage team (still more bug)

---
+++ src/main/java/cs211/project/componentControllers/teamControllers/manageTeamController/ManageMemberTeamListController.java
@@ -0,0 +1,43 @@
+package cs211.project.componentControllers.teamControllers.manageTeamController;
+
+import cs211.project.models.Event;
+import cs211.project.models.User;
+import cs211.project.models.collections.TeamList;
+import cs211.project.services.FXRouter;
+import cs211.project.services.JoinTeamMap;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+public class ManageMemberTeamListController {
+    private User user = (User) FXRouter.getData();
+    private Event event = (Event) FXRouter.getData2();
+
+    @FXML private ImageView roleImageView, statusImageView, menuImageView;
+    @FXML private Label nameLabel, roleLabel, statusLabel, menuLabel;
+    protected Image roleIcon, statusIcon, menuIcon;
+
+    JoinTeamMap joinTeamMap = new JoinTeamMap();
+    HashMap<String, TeamList> teamListHashMap;
+    TeamList teamList;
+
+    @FXML private void initialize() {
+        loadImageInit();
+    }
+
+    private void loadImageInit(){
+        roleIcon = new Image(getClass().getResourceAsStream("/images/icons/team-box/role/Leader.png"));
+        statusIcon = new Image(getClass().getResourceAsStream("/images/icons/team-box/active_icon.png"));
+        menuIcon = new Image(getClass().getResourceAsStream("/images/icons/team-box/dot_icon.png"));
+
+        roleImageView.setImage(roleIcon);
+        statusImageView.setImage(statusIcon);
+        menuImageView.setImage(menuIcon);
+    }
+}
---
+++ src/main/java/cs211/project/componentControllers/teamControllers/manageTeamController/ManageTeamController.java
@@ -0,0 +1,78 @@
+package cs211.project.componentControllers.teamControllers.manageTeamController;
+
+import cs211.project.models.User;
+import cs211.project.models.collections.TeamList;
+import cs211.project.models.collections.UserList;
+import cs211.project.services.FXRouter;
+import cs211.project.services.JoinTeamMap;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+public class ManageTeamController {
+    private User user = (User) FXRouter.getData();
+
+    GridPane memberContainer;
+    JoinTeamMap joinTeamMap = new JoinTeamMap();
+    HashMap<String, UserList> userListHashMap;
+    UserList userList;
+
+
+    @FXML private void initialize(){
+        showUserList();
+    }
+
+    private void showUserList() {
+        userListHashMap = joinTeamMap.roleReadData();
+        userList = userListHashMap.get(user.getTeamJoined());
+        for (int i = 0; i < userList.getUsers().size(); i++) {
+
+            loadManageTeamComponent(userList.getUsers().get(i), 0, i);
+        }
+
+
+    }
+
+    private void loadManageTeamComponent(User user, int col, int row) {
+        FXMLLoader manageTeamLoader = new FXMLLoader(getClass().getResource("cs211/project/views/components/team/manage-team/manage-team.fxml"));
+        AnchorPane manageTeamComponent;
+        if (user != null) {
+            try {
+                manageTeamComponent = manageTeamLoader.load();
+                Label nameLabel = (Label) manageTeamComponent.getChildren().get(0);
+                AnchorPane roleAnchorPane = (AnchorPane) manageTeamComponent.getChildren().get(1);
+                ImageView roleImageView = (ImageView) roleAnchorPane.getChildren().get(0);
+                Label roleLabel = (Label) roleAnchorPane.getChildren().get(1);
+                ImageView menuImageView = (ImageView) manageTeamComponent.getChildren().get(2);
+                // Label bookmarkLabel = (Label) manageTeamComponent.getChildren().get(3);
+                Label idLabel = (Label) manageTeamComponent.getChildren().get(4);
+                Label statusLabel = (Label) manageTeamComponent.getChildren().get(5);
+                ImageView statusImageView = (ImageView) manageTeamComponent.getChildren().get(6);
+
+                nameLabel.setText(user.getUsername());
+                roleLabel.setText(user.getRole());
+                idLabel.setText(user.getUserId());
+                statusLabel.setText((user.getStatus())? "Online" : "Offline");
+                roleImageView.setImage(new Image(getClass().getResourceAsStream("/images/icons/team-box/role/" + user.getRole() + ".png")));
+                menuImageView.setImage(new Image(getClass().getResourceAsStream("/images/icons/team-box/dot_icon.png")));
+                statusImageView.setImage(new Image(getClass().getResourceAsStream("/images/icons/team-box/active_icon.png")));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        } else {
+            return;
+        }
+
+        GridPane.setMargin(manageTeamComponent,new Insets(0,0,5,0));
+
+        memberContainer.add(manageTeamComponent, col, row);
+    }
+}
