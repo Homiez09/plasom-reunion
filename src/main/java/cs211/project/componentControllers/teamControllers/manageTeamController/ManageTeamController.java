9e19355ce740b5e33165b6b509d1b0e60f8a3f9a
MalaMing <minggggggg@gmail.com>
2023-09-22 07:00:16 +0700

    add manage team feature

--- src/main/java/cs211/project/componentControllers/teamControllers/manageTeamController/ManageTeamController.java
+++ src/main/java/cs211/project/componentControllers/teamControllers/manageTeamController/ManageTeamController.java
@@ -2,7 +2,6 @@ package cs211.project.componentControllers.teamControllers.manageTeamController;
 
 import cs211.project.models.Event;
 import cs211.project.models.User;
-import cs211.project.models.collections.TeamList;
 import cs211.project.models.collections.UserList;
 import cs211.project.services.FXRouter;
 import cs211.project.services.JoinTeamMap;
@@ -21,29 +20,29 @@ import java.util.HashMap;
 public class ManageTeamController {
     private User user = (User) FXRouter.getData();
     private Event event = (Event) FXRouter.getData2();
+
     @FXML private GridPane memberContainer;
+
     JoinTeamMap joinTeamMap = new JoinTeamMap();
     HashMap<String, UserList> userListHashMap;
     UserList userList;
 
 
-    @FXML private void initialize(){
-        showUserList();
-    }
+    @FXML private void initialize(){}
 
-    private void showUserList() {
-        userListHashMap = joinTeamMap.roleReadData();
-        userList = userListHashMap.get(user.getTeamJoined());
-        for (int i = 0; i < userList.getUsers().size(); i++) {
-            System.out.println(i);
-            loadManageTeamComponent(userList.getUsers().get(i), 0, i);
+    public void showUserList(String teamID) {
+        JoinTeamMap joinTeamMap = new JoinTeamMap();
+        HashMap<String, UserList> hashMap = joinTeamMap.roleReadData();
+        if (hashMap.isEmpty()) return;
+        UserList memberList = hashMap.get(teamID);
+        for (User user : memberList.getUsers()) {
+            loadManageTeamComponent(user, 0, memberList.getUsers().indexOf(user));
         }
-
 
     }
 
     private void loadManageTeamComponent(User user, int col, int row) {
-        FXMLLoader manageTeamLoader = new FXMLLoader(getClass().getResource("cs211/project/views/components/team/manage-team/manage-team.fxml"));
+        FXMLLoader manageTeamLoader = new FXMLLoader(getClass().getResource("/cs211/project/views/components/team/manage-team/manage-member-team-list.fxml"));
         AnchorPane manageTeamComponent;
         if (user != null) {
             try {
@@ -53,7 +52,6 @@ public class ManageTeamController {
                 ImageView roleImageView = (ImageView) roleAnchorPane.getChildren().get(0);
                 Label roleLabel = (Label) roleAnchorPane.getChildren().get(1);
                 ImageView menuImageView = (ImageView) manageTeamComponent.getChildren().get(2);
-                // Label bookmarkLabel = (Label) manageTeamComponent.getChildren().get(3);
                 Label idLabel = (Label) manageTeamComponent.getChildren().get(4);
                 Label statusLabel = (Label) manageTeamComponent.getChildren().get(5);
                 ImageView statusImageView = (ImageView) manageTeamComponent.getChildren().get(6);
@@ -61,21 +59,28 @@ public class ManageTeamController {
                 nameLabel.setText(user.getUsername());
                 roleLabel.setText(user.getRole());
                 idLabel.setText(user.getUserId());
-                statusLabel.setText((user.getStatus())? "Online" : "Offline");
+                statusLabel.setText((user.getStatus())? "online" : "offline");
                 roleImageView.setImage(new Image(getClass().getResourceAsStream("/images/icons/team-box/role/" + user.getRole() + ".png")));
                 menuImageView.setImage(new Image(getClass().getResourceAsStream("/images/icons/team-box/dot_icon.png")));
-                statusImageView.setImage(new Image(getClass().getResourceAsStream("/images/icons/team-box/active_icon.png")));
+                statusImageView.setImage(new Image(getClass().getResourceAsStream("/images/icons/login/status/" + statusLabel.getText() + "_active.png")));
+
+
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
         } else {
             return;
         }
-
         GridPane.setMargin(manageTeamComponent,new Insets(0,0,5,0));
-
         memberContainer.add(manageTeamComponent, col, row);
     }
 
-    @FXML private void onBackClick() {}
+
+    @FXML private void onBackClick() {
+        try {
+            FXRouter.goTo("select-team", user, event);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
