6eef2abbb4704534010b182f205c60bdfee67936
MalaMing <minggggggg@gmail.com>
2023-09-22 15:10:06 +0700

    fix bug and update CSV files

--- src/main/java/cs211/project/controllers/SelectTeamController.java
+++ src/main/java/cs211/project/controllers/SelectTeamController.java
@@ -1,7 +1,6 @@
 package cs211.project.controllers;
 
 import cs211.project.componentControllers.teamControllers.manageTeamController.ManageTeamController;
-import cs211.project.componentControllers.teamboxControllers.TeamBox1Controller;
 import cs211.project.models.Team;
 import cs211.project.models.User;
 import cs211.project.models.Event;
@@ -204,6 +203,8 @@ public class SelectTeamController {
                     teamBoxComponent = teamBoxLoader2.load();
                 }
 
+                loadTeamBoxComponent(team, 0, row2++);
+
                 Label teamID = (Label) teamBoxComponent.getChildren().get(1);
                 Label teamName = (Label)teamBoxComponent.getChildren().get(6);
                 ImageView bookMarkImageView = (ImageView) teamBoxComponent.getChildren().get(5);
@@ -225,7 +226,6 @@ public class SelectTeamController {
 
                 menuDropDown.getSelectionModel().selectedItemProperty().addListener((v, oldValue, newValue) -> {
                     if (newValue == null) return;
-                    TeamBox1Controller teamBox1Controller = teamBoxLoader1.getController();
                     if (newValue.equals("Manage Team")) {
                         manageTeamSelectMenuGraphic(1);
 
@@ -236,12 +236,6 @@ public class SelectTeamController {
 
                         initManageTeam();
                         showManageTeam();
-                    }
-
-                    try {
-                        teamBox1Controller.goTo((String) newValue);
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
                     }
                 });
 
@@ -259,20 +253,8 @@ public class SelectTeamController {
                     column = 0;
                     row++;
                 }
-
-//                teamBoxComponent.setOnMouseClicked(e -> {
-//                    try {
-//                        FXRouter.goTo("team", user, event, team); // todo : If you are developer, You don't forget to change routeLabel link to your page and "getData, getData2, getData3" by "User, Event, Team"
-//                    // todo : If you have back button link to select-team --> FXRouter.goTO("select-team", user, event)
-//                    } catch (IOException ex) {
-//                        throw new RuntimeException(ex);
-//                    }
-//                });
-
                 GridPane.setMargin(teamBoxComponent,new Insets(18,18,37,33));
-
                 teamContainer.add(teamBoxComponent, ++column, row);
-                loadTeamBoxComponent(team, 0, row2++); // load manage teams
             } catch (IOException e) {
                 e.printStackTrace();
             }
