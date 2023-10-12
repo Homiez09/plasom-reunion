a38b5f935ea3a1a01794d3a0bcc5e97a35e4c642
JindamaneeSri <jindamanee.sri@ku.th>
2023-10-13 01:35:10 +0700

    instruction for normal user

--- src/main/java/cs211/project/controllers/view/InstructionController.java
+++ src/main/java/cs211/project/controllers/view/InstructionController.java
@@ -3,6 +3,7 @@ package cs211.project.controllers.view;
 import cs211.project.services.LoadTopBarComponent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.AnchorPane;
 
 import java.io.IOException;
@@ -10,50 +11,65 @@ import java.io.IOException;
 public class InstructionController {
     @FXML private AnchorPane topBarAnchorPane;
     @FXML private AnchorPane instructionAnchorPane;
+    @FXML private ScrollPane instructionScrollPane;
 
     @FXML private void initialize() {
         new LoadTopBarComponent(topBarAnchorPane);
-        loadGuest();
+        loadInstruction("guest");
     }
 
     @FXML private void onGuestButtonClick() {
-        loadGuest();
+        loadInstruction("guest");
     }
     @FXML private void onUserButtonClick() {
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/cs211/project/views/components/instruction/info-user.fxml"));
-            AnchorPane loaded = loader.load();
-            instructionAnchorPane.getChildren().setAll(loaded);
-        }catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        loadInstruction("user");
     }
     @FXML private void onOrganizerButtonClick() {
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/cs211/project/views/components/instruction/organizer.fxml"));
-            AnchorPane loaded = loader.load();
-            instructionAnchorPane.getChildren().setAll(loaded);
-        }catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+       loadInstruction("organizer");
     }
     @FXML private void onTeamButtonClick() {
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/cs211/project/views/components/instruction/team-staff.fxml"));
-            AnchorPane loaded = loader.load();
-            instructionAnchorPane.getChildren().setAll(loaded);
-        }catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        loadInstruction("team");
     }
 
-    private void loadGuest() {
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/cs211/project/views/components/instruction/info-guest.fxml"));
-            AnchorPane loaded = loader.load();
-            instructionAnchorPane.getChildren().setAll(loaded);
-        }catch (IOException e) {
-            throw new RuntimeException(e);
+    private void loadInstruction(String page) {
+        switch (page) {
+            case "guest" :
+                try {
+                    FXMLLoader loader = new FXMLLoader(getClass().getResource("/cs211/project/views/components/instruction/info-guest.fxml"));
+                    AnchorPane loaded = loader.load();
+                    instructionAnchorPane.getChildren().setAll(loaded);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                break;
+            case "user" :
+                try {
+                    FXMLLoader loader = new FXMLLoader(getClass().getResource("/cs211/project/views/components/instruction/info-user.fxml"));
+                    AnchorPane loaded = loader.load();
+                    instructionAnchorPane.getChildren().setAll(loaded);
+                }catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                break;
+            case "organizer" :
+                try {
+                    FXMLLoader loader = new FXMLLoader(getClass().getResource("/cs211/project/views/components/instruction/organizer.fxml"));
+                    AnchorPane loaded = loader.load();
+                    instructionAnchorPane.getChildren().setAll(loaded);
+                }catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                break;
+            case "team" :
+                try {
+                    FXMLLoader loader = new FXMLLoader(getClass().getResource("/cs211/project/views/components/instruction/team-staff.fxml"));
+                    AnchorPane loaded = loader.load();
+                    instructionAnchorPane.getChildren().setAll(loaded);
+                }catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                break;
         }
+        instructionScrollPane.setVvalue(0);
     }
 }
